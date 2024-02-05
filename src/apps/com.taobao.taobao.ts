04417bdce26294bdf75b0330c4a311d082a8cb9a
aoguai <1340219674@qq.com>
2024-02-05 20:49:41 +0800

    fix(淘宝): 将小组件添加到手机桌面

--- src/apps/com.taobao.taobao.ts
+++ src/apps/com.taobao.taobao.ts
@@ -172,13 +172,12 @@ export default defineAppConfig({
       key: 14,
       name: '全屏广告-将小组件添加到手机桌面',
       desc: '点击取消',
+      enable: false,
       activityIds: 'com.alibaba.triver.container.TriverMainActivity',
-      rules: [
-        {
-          action: 'clickCenter',
-          matches: 'View[text="立即添加"] + View[text="取消"]',
-          snapshotUrls: 'https://i.gkd.li/import/13598578',
-        },
+      rules: 'View[childCount=2] >3 View[text="立即添加"] + View[text="取消"]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13598578',
+        'https://i.gkd.li/import/13853510', //误触快照
       ],
     },
     {
