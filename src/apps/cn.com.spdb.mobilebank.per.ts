6168eb372cf38571718f53e144329c0595ad7c7e
aoguai <1340219674@qq.com>
2024-02-05 14:10:33 +0800

    fix(浦发银行): 消息中心-系统通知请求

--- src/apps/cn.com.spdb.mobilebank.per.ts
+++ src/apps/cn.com.spdb.mobilebank.per.ts
@@ -9,11 +9,17 @@ export default defineAppConfig({
       key: 1,
       name: '通知提示-消息中心-系统通知请求',
       desc: '自动点击关闭。',
+      quickFind: true,
+      actionMaximum: 1, // 限制只能点击一次
+      resetMatch: 'app',
       enable: false,
       activityIds:
         'cn.com.spdb.mobilebank.per.activity.web.stage.StageCommonWebActivity',
       rules: '[text="去开启"] + View > Image[text.length=0]',
-      snapshotUrls: 'https://i.gkd.li/import/13458535',
+      snapshotUrls: [
+        'https://i.gkd.li/import/13458535',
+        'https://i.gkd.li/import/14161174', // 点击后元素并不会消失
+      ],
     },
   ],
 });
