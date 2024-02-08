1c539c95535b13681a63a9df0c9ef7a0cdb60b10
aoguai <1340219674@qq.com>
2024-02-08 18:26:04 +0800

    chore: Update NEGATION_PART_RULE_TEXT, NEGATION_PART_RULE_DESC

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -72,10 +72,10 @@ const uniqueAppIdsRp = new Set([
   ...filterAppsByGroup(apps, '评价提示'),
 ]);
 
-const NEGATION_PART_RULE_TEXT =
-  '[childCount=0][visibleToUser=true][childCount=0][visibleToUser=true][text^="不了"||text^="不再"||text^="忽略"||text^="暂不"||text^="放弃"||text^="取消"||text$="再说"||text$="再想想"||text$="拒绝"||text$="再說"||text$="拒絕"||text^="暫不"||text^="Ignore"||text^="Not now"||text^="Cancel"||text$="later"||text$="refuse"]';
-const NEGATION_PART_RULE_DESC =
-  '[childCount=0][visibleToUser=true][childCount=0][visibleToUser=true][desc^="不了"||desc^="不再"||desc^="忽略"||desc^="暂不"||desc^="放弃"||desc^="取消"||desc$="再说"||desc$="再想想"||desc$="拒绝"||desc$="再說"||desc$="拒絕"||desc^="暫不"||desc^="Ignore"||desc^="Not now"||desc^="Cancel"||desc$="later"||desc$="refuse"]';
+const COMMON_PREFIX = '[childCount=0][visibleToUser=true]';
+
+const NEGATION_PART_RULE_TEXT = `${COMMON_PREFIX}[((text^="不"&&text$="谢谢")||text="否"||text="关闭"||text="不开启"||text="暂时不用"||text="先不了"||text="不允许"||text^="不了"||text^="不再"||text^="忽略"||text^="暂不"||text^="放弃"||text^="取消"||text$="再说"||text$="拒绝"||text$="再想想"||text$="知道了"||(text^="不"&&text$="謝謝")||text="關閉"||text="不開啟"||text="關閉"||text$="再說"||text$="拒絕"||text^="暫不"||text="close"||text="not now"||text^="Ignore"||text^="Cancel"||text$="later"||text$="refuse"||text$="I see")&&text.length<=7]`;
+const NEGATION_PART_RULE_DESC = `${COMMON_PREFIX}[((desc^="不"&&desc$="谢谢")||desc="否"||desc="关闭"||desc="不开启"||desc="暂时不用"||desc="先不了"||desc="不允许"||desc^="不了"||desc^="不再"||desc^="忽略"||desc^="暂不"||desc^="放弃"||desc^="取消"||desc$="再说"||desc$="拒绝"||desc$="再想想"||desc$="知道了"||(desc^="不"&&desc$="謝謝")||desc="關閉"||desc="不開啟"||desc="關閉"||desc$="再說"||desc$="拒絕"||desc^="暫不"||desc="close"||desc="not now"||desc^="Ignore"||desc^="Cancel"||desc$="later"||desc$="refuse"||desc$="I see")&&desc.length<=7]`;
 
 const globalGroups: RawGlobalGroup[] = [
   {
