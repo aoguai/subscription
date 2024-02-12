2419ef52eef024d363219ae53b22737818d40639
aoguai <1340219674@qq.com>
2024-02-12 19:16:15 +0800

    feat(全局规则): 局部广告

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -63,6 +63,14 @@ const uniqueAppIdsAD = new Set([
   ...diabledAppIds,
   ...filterAppsByGroup(apps, '开屏广告'),
 ]);
+const uniqueAppIdsFA = new Set([
+  ...diabledAppIds,
+  ...filterAppsByGroup(apps, '全屏广告'),
+]);
+const uniqueAppIdsPA = new Set([
+  ...diabledAppIds,
+  ...filterAppsByGroup(apps, '局部广告'),
+]);
 const uniqueAppIdsUP = new Set([
   ...diabledAppIds,
   ...filterAppsByGroup(apps, '更新提示'),
@@ -83,10 +91,6 @@ const uniqueAppIdsPP = new Set([
   ...diabledAppIds,
   ...filterAppsByGroup(apps, '权限提示'),
 ]);
-const uniqueAppIdsFA = new Set([
-  ...diabledAppIds,
-  ...filterAppsByGroup(apps, '全屏广告'),
-]);
 const uniqueAppIdsLP = new Set([
   ...diabledAppIds,
   ...filterAppsByGroup(apps, '定位提示'),
@@ -96,7 +100,7 @@ const COMMON_PREFIX = '[childCount=0][visibleToUser=true]';
 
 const NEGATION_PART_RULE_TEXT = `${COMMON_PREFIX}[((text^="不"&&text$="谢谢")||text="否"||text="关闭"||text="不开启"||text="暂时不用"||text="考虑一下"||text="考慮一下"||text="先不了"||text="不允许"||text^="不了"||text^="不再"||text^="忽略"||text^="暂不"||text^="放弃"||text^="取消"||text$="再说"||text$="拒绝"||text$="再想想"||text$="知道了"||(text^="不"&&text$="謝謝")||text="關閉"||text="不開啟"||text="關閉"||text$="再說"||text$="拒絕"||text^="暫不"||text="close"||text="Close"||text="Not now"||text="not now"||text^="Ignore"||text^="Lgnore"||text^="Cancel"||text^="cancel"||text$="later"||text$="Later"||text$="refuse"||text$="Refuse"||text$="i see"||text$="I see")&&text.length<=7]`;
 const NEGATION_PART_RULE_DESC = `${COMMON_PREFIX}[((desc^="不"&&desc$="谢谢")||desc="否"||desc="关闭"||desc="不开启"||desc="暂时不用"||desc="考虑一下"||desc="考慮一下"||desc="先不了"||desc="不允许"||desc^="不了"||desc^="不再"||desc^="忽略"||desc^="暂不"||desc^="放弃"||desc^="取消"||desc$="再说"||desc$="拒绝"||desc$="再想想"||desc$="知道了"||(desc^="不"&&desc$="謝謝")||desc="關閉"||desc="不開啟"||desc="關閉"||desc$="再說"||desc$="拒絕"||desc^="暫不"||desc="close"||desc="Close"||desc="Not now"||desc="not now"||desc^="Ignore"||desc^="Lgnore"||desc^="Cancel"||desc^="cancel"||desc$="later"||desc$="Later"||desc$="refuse"||desc$="Refuse"||desc$="i see"||desc$="I see")&&desc.length<=7]`;
-const NEGATION_PART_RULE_BUTTON = `${COMMON_PREFIX}[(id*="iv"||id*="guide"||id*="alert"||id*="Notific"||id*="dialog"||id*="btn"||id*="ad"||id*="ab")&&(id$="close"||id$="Close"||id$="Delete"||id$="delete"||id$="cancel"||id$="Cancel"||id$="cancle"||id$="Cancle"||id$="exit"||id$="Exit")||id*="/close"||id*="/Close"||id*="_close"||id*="_Close"||id*="/ab"||text=""||desc=""]`;
+const NEGATION_PART_RULE_BUTTON = `${COMMON_PREFIX}[(id*="iv"||id*="guide"||id*="alert"||id*="Notific"||id*="dialog"||id*="btn"||id*="ad"||id*="ab")&&(id$="close"||id$="Close"||id$="Delete"||id$="delete"||id$="cancel"||id$="Cancel"||id$="cancle"||id$="Cancle"||id$="exit"||id$="Exit")||id*="/close"||id*="/Close"||id*="/ab"||id*="/deleteIv"||id*="_close"||id*="_Close"||text=""||desc=""||text="×"||desc="×"]`;
 
 const UP_commonTextPatterns =
   '[text$="新版本"||text$="更新"||text$="升级"||text$="体验"||text$="升級"||text$="體驗"||text$="Update"||text$="Upgrade"||text$="Experience"]';
@@ -128,6 +132,11 @@ const LP_commonTextPatterns =
 const LP_commonDescPatterns =
   '[(desc*="访问"||desc*="申请"||desc*="开启"||desc*="打开"||desc*="获取")&&(desc*="定位"||desc*="位置"||desc*="location")&&desc!*="通知"]';
 
+const PA_commonTextPatterns =
+  '[text$="广告"||text$="廣告"||text$="AD"||text="ad"||(text*="申请"||text*="开启"||text*="打开"||text*="获取"||text*="订阅"||text*="接收"||text*="Turn on")&&(text*="个性化"||text*="推荐"||text*="感兴趣"||text*="個性化"||text*="推薦"||text*="感興趣"||text*="感興趣")]';
+const PA_commonDescPatterns =
+  '[desc$="广告"||desc$="廣告"||desc$="AD"||desc="ad"||(desc*="申请"||desc*="开启"||desc*="打开"||desc*="获取"||desc*="订阅"||desc*="接收"||desc*="Turn on")&&(desc*="个性化"||desc*="推荐"||desc*="感兴趣"||desc*="個性化"||desc*="推薦"||desc*="感興趣"||desc*="感興趣")]';
+
 const globalGroups: RawGlobalGroup[] = [
   {
     key: 0,
@@ -155,6 +164,112 @@ const globalGroups: RawGlobalGroup[] = [
   },
   {
     key: 1,
+    name: '全屏广告',
+    enable: false,
+    order: utils.FULLSCREEN_AD,
+    matchTime: 10000,
+    resetMatch: 'activity',
+    rules: [
+      {
+        key: 0,
+        name: '快手SDK-类型1',
+        matches: `[text="广告"] <<n ViewGroup >n ViewGroup[childCount=1][clickable=true] > ImageView${COMMON_PREFIX}`,
+      },
+      {
+        key: 1,
+        name: '快手SDK-类型2',
+        matches: `[text="广告"] <<n ViewGroup >n ViewGroup[childCount=4][clickable=true] > [text="跳过"]${COMMON_PREFIX}`,
+      },
+      {
+        key: 2,
+        name: '快手SDK-类型3',
+        matches: `[text="广告"]  <<n ViewGroup +2 ViewGroup[childCount=3][checked=false] >n ImageView${COMMON_PREFIX}`,
+      },
+      {
+        key: 3,
+        name: '字节SDK-类型1',
+        matches: `[id$="tt_reward_full_count_down_after_close"]${COMMON_PREFIX}`,
+      },
+      {
+        key: 4,
+        name: '字节SDK-类型2',
+        matches: `[id$="ad_flag_source_layout"] + [id$="iv_close"]${COMMON_PREFIX}`,
+      },
+      {
+        key: 5,
+        name: '美数SDK-类型1',
+        matches: `[id$="ms_activity_sdk_interstitial_cacel"]${COMMON_PREFIX}`,
+      },
+    ],
+    // 将 Set 转换为数组，并设置 enable 为 false
+    apps: [...uniqueAppIdsFA].map((id) => ({ id, enable: false })),
+  },
+  {
+    key: 2,
+    name: '局部广告',
+    enable: false,
+    order: utils.PARTIAL_AD,
+    matchTime: 10000,
+    resetMatch: 'activity',
+    rules: [
+      {
+        key: 0,
+        matches: `[(((id*="/ad"||id*="/AD"||id$="ad"||id$="Ad"||id$="AD")&&id*="_")||id$="/adIv"||id$="_ad_"||id$="_Ad_"||id$="_AD_")&&(id!*="download"&&id!*="Download"&&id!*="DOWNLOAD")] <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 1,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > ${NEGATION_PART_RULE_TEXT}`,
+      },
+      {
+        key: 2,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > * >n ${NEGATION_PART_RULE_TEXT}`,
+      },
+      {
+        key: 3,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_TEXT}`,
+      },
+      {
+        key: 4,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > ${NEGATION_PART_RULE_DESC}`,
+      },
+      {
+        key: 5,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > * >n ${NEGATION_PART_RULE_DESC}`,
+      },
+      {
+        key: 6,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_DESC}`,
+      },
+      {
+        key: 7,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 8,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 9,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 10,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 11,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 12,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+    ],
+    // 将 Set 转换为数组，并设置 enable 为 false
+    apps: [...uniqueAppIdsPA].map((id) => ({ id, enable: false })),
+  },
+  {
+    key: 3,
     name: '更新提示',
     enable: false,
     order: utils.UPDATE_PROMPT,
@@ -217,7 +332,7 @@ const globalGroups: RawGlobalGroup[] = [
     apps: [...uniqueAppIdsUP].map((id) => ({ id, enable: false })),
   },
   {
-    key: 2,
+    key: 4,
     name: '评价提示',
     enable: false,
     order: utils.REVIEW_PROMPT,
@@ -280,7 +395,7 @@ const globalGroups: RawGlobalGroup[] = [
     apps: [...uniqueAppIdsRP].map((id) => ({ id, enable: false })),
   },
   {
-    key: 3,
+    key: 5,
     name: '通知提示',
     enable: false,
     order: utils.NOTIFICATION_PROMPT,
@@ -342,7 +457,7 @@ const globalGroups: RawGlobalGroup[] = [
     apps: [...uniqueAppIdsNP].map((id) => ({ id, enable: false })),
   },
   {
-    key: 4,
+    key: 6,
     name: '青少年模式',
     enable: false,
     order: utils.YOUTH_MODE,
@@ -405,7 +520,7 @@ const globalGroups: RawGlobalGroup[] = [
     apps: [...uniqueAppIdsYM].map((id) => ({ id, enable: false })),
   },
   {
-    key: 5,
+    key: 7,
     name: '权限提示',
     desc: '! 该规则会自动拒绝 APP 一些权限申请弹窗提示，如果有影响请关闭',
     enable: false,
@@ -468,49 +583,7 @@ const globalGroups: RawGlobalGroup[] = [
     apps: [...uniqueAppIdsPP].map((id) => ({ id, enable: false })),
   },
   {
-    key: 6,
-    name: '全屏广告',
-    enable: false,
-    order: utils.FULLSCREEN_AD,
-    matchTime: 10000,
-    resetMatch: 'activity',
-    rules: [
-      {
-        key: 0,
-        name: '快手SDK-类型1',
-        matches: `[text="广告"] <<n ViewGroup >n ViewGroup[childCount=1][clickable=true] > ImageView${COMMON_PREFIX}`,
-      },
-      {
-        key: 1,
-        name: '快手SDK-类型2',
-        matches: `[text="广告"] <<n ViewGroup >n ViewGroup[childCount=4][clickable=true] > [text="跳过"]${COMMON_PREFIX}`,
-      },
-      {
-        key: 2,
-        name: '快手SDK-类型3',
-        matches: `[text="广告"]  <<n ViewGroup +2 ViewGroup[childCount=3][checked=false] >n ImageView${COMMON_PREFIX}`,
-      },
-      {
-        key: 3,
-        name: '字节SDK-类型1',
-        matches: `[id$="tt_reward_full_count_down_after_close"]${COMMON_PREFIX}`,
-      },
-      {
-        key: 4,
-        name: '字节SDK-类型2',
-        matches: `[id$="ad_flag_source_layout"] + [id$="iv_close"]${COMMON_PREFIX}`,
-      },
-      {
-        key: 5,
-        name: '美数SDK-类型1',
-        matches: `[id$="ms_activity_sdk_interstitial_cacel"]${COMMON_PREFIX}`,
-      },
-    ],
-    // 将 Set 转换为数组，并设置 enable 为 false
-    apps: [...uniqueAppIdsFA].map((id) => ({ id, enable: false })),
-  },
-  {
-    key: 7,
+    key: 8,
     name: '定位提示',
     desc: '! 该规则会自动拒绝 APP 的位置权限申请弹窗提示，如果有影响请关闭',
     enable: false,
