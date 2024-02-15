4cd9cec65ceeb7469ab92490cb1bef3159d507be
aoguai <1340219674@qq.com>
2024-02-15 14:02:29 +0800

    fix(闲鱼): 通知提示

--- src/apps/com.taobao.idlefish.ts
+++ src/apps/com.taobao.idlefish.ts
@@ -7,8 +7,9 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '通知提示-通知提示',
+      name: '通知提示',
       actionMaximum: 1,
+      resetMatch: 'app',
       activityIds:
         'com.idlefish.flutterbridge.flutterboost.boost.FishFlutterBoostTransparencyActivity',
       rules: '[desc^="开启系统通知"] > ImageView[clickable=true][desc=null]',
