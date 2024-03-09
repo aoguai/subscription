27d42c487db1a1522166927bf9e2fa8e4aa402ce
aoguai <1340219674@qq.com>
2024-03-09 22:12:45 +0800

    feat: 全局规则-通知提示 使用 matches 优化

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -46,14 +46,9 @@ const uniqueAppIdsYM = new Set([
 
 const COMMON_PREFIX = '[name!$=".CheckBox"][childCount=0][visibleToUser=true]';
 
-const NEGATION_PART_RULE_TEXT = `${COMMON_PREFIX}[((text^="不"&&text$="谢谢")||text="否"||text="关闭"||text="关闭按钮"||text="不开启"||text="暂时不用"||text="我已知晓"||text="不用了"||text="本次忽略"||text="考虑一下"||text="考慮一下"||text="先不了"||text="不允许"||text^="不了"||text^="不再"||text^="稍后"||text^="忽略"||text^="暂不"||text^="放弃"||text^="取消"||text$="再说"||text$="拒绝"||text$="再想想"||text$="知道了"||text$="稍后提醒我"||text$="稍後提醒我"||(text^="不"&&text$="謝謝")||text="關閉"||text="關閉按鈕"||text="我已知曉"||text="不開啟"||text$="再說"||text$="拒絕"||text^="暫不"||text="close"||text="Close"||text="Not now"||text="not now"||text^="Ignore"||text^="Lgnore"||text^="Cancel"||text^="cancel"||text$="later"||text$="Later"||text$="LATER"||text$="refuse"||text$="Refuse"||text$="REFUSE"||text$="i see"||text$="I see"||text$="I SEE")&&text.length<=7]`;
-const NEGATION_PART_RULE_DESC = `${COMMON_PREFIX}[((desc^="不"&&desc$="谢谢")||desc="否"||desc="关闭"||desc="关闭按钮"||desc="不开启"||desc="暂时不用"||desc="我已知晓"||desc="不用了"||desc="本次忽略"||desc="考虑一下"||desc="考慮一下"||desc="先不了"||desc="不允许"||desc^="不了"||desc^="不再"||desc^="稍后"||desc^="忽略"||desc^="暂不"||desc^="放弃"||desc^="取消"||desc$="再说"||desc$="拒绝"||desc$="再想想"||desc$="知道了"||desc$="稍后提醒我"||desc$="稍後提醒我"||(desc^="不"&&desc$="謝謝")||desc="關閉"||desc="關閉按鈕"||desc="我已知曉"||desc="不開啟"||desc$="再說"||desc$="拒絕"||desc^="暫不"||desc="close"||desc="Close"||desc="Not now"||desc="not now"||desc^="Ignore"||desc^="Lgnore"||desc^="Cancel"||desc^="cancel"||desc$="later"||desc$="Later"||desc$="LATER"||desc$="refuse"||desc$="Refuse"||desc$="REFUSE"||desc$="i see"||desc$="I see"||desc$="I SEE")&&desc.length<=7]`;
+const NEGATION_PART_RULE_TEXT = `${COMMON_PREFIX}[((text^="不"&&text$="谢谢")||text="否"||text="关闭"||text="跳过"||text="关闭按钮"||text="不开启"||text="暂时不用"||text="暂时不要"||text="我已知晓"||text="不用了"||text="本次忽略"||text="考虑一下"||text="考慮一下"||text="先不了"||text="不允许"||text^="不了"||text^="不再"||text^="稍后"||text^="忽略"||text^="暂不"||text^="放弃"||text^="取消"||text$="再说"||text$="拒绝"||text$="再想想"||text$="知道了"||text$="稍后提醒我"||text$="稍後提醒我"||(text^="不"&&text$="謝謝")||text="關閉"||text="關閉按鈕"||text="我已知曉"||text="不開啟"||text$="再說"||text$="拒絕"||text^="暫不"||text="close"||text="Close"||text="Not now"||text="not now"||text^="Ignore"||text^="Lgnore"||text^="Cancel"||text^="cancel"||text$="later"||text$="Later"||text$="LATER"||text$="refuse"||text$="Refuse"||text$="REFUSE"||text$="i see"||text$="I see"||text$="I SEE")&&text.length<=7]`;
+const NEGATION_PART_RULE_DESC = `${COMMON_PREFIX}[((desc^="不"&&desc$="谢谢")||desc="否"||desc="关闭"||desc="跳过"||desc="关闭按钮"||desc="不开启"||desc="暂时不用"||desc="暂时不要"||desc="我已知晓"||desc="不用了"||desc="本次忽略"||desc="考虑一下"||desc="考慮一下"||desc="先不了"||desc="不允许"||desc^="不了"||desc^="不再"||desc^="稍后"||desc^="忽略"||desc^="暂不"||desc^="放弃"||desc^="取消"||desc$="再说"||desc$="拒绝"||desc$="再想想"||desc$="知道了"||desc$="稍后提醒我"||desc$="稍後提醒我"||(desc^="不"&&desc$="謝謝")||desc="關閉"||desc="關閉按鈕"||desc="我已知曉"||desc="不開啟"||desc$="再說"||desc$="拒絕"||desc^="暫不"||desc="close"||desc="Close"||desc="Not now"||desc="not now"||desc^="Ignore"||desc^="Lgnore"||desc^="Cancel"||desc^="cancel"||desc$="later"||desc$="Later"||desc$="LATER"||desc$="refuse"||desc$="Refuse"||desc$="REFUSE"||desc$="i see"||desc$="I see"||desc$="I SEE")&&desc.length<=7]`;
 const NEGATION_PART_RULE_BUTTON = `${COMMON_PREFIX}[(vid*="iv"||vid*="guide"||vid*="alert"||vid*="Notific"||vid*="dialog"||vid*="btn"||vid*="ad"||vid*="ab")&&(vid$="close"||vid$="Close"||vid$="Delete"||vid$="delete"||vid$="cancel"||vid$="Cancel"||vid$="cancle"||vid$="Cancle"||vid$="exit"||vid$="Exit")||vid^="close"||vid^="Close"||vid^="ab"||vid^="closeIv"||vid^="deleteIv"||vid*="_close"||vid*="_Close"||text=""||desc=""||text="×"||desc="×"||text="퀺"||desc="퀺"]`;
-
-const NP_commonTextPatterns =
-  '[(text*="是否允许"||text*="申请"||text*="开启"||text*="打开"||text*="获取"||text*="订阅"||text*="接收"||text*="Turn on"||text*="turn on")&&(text*="通知"||text*="推送"||text*="notifications"||text*="Notifications")&&(text!*="定位"&&text!*="位置"&&text!*="location"&&text!*="权限"&&text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))]';
-const NP_commonDescPatterns =
-  '[(desc*="是否允许"||desc*="申请"||desc*="开启"||desc*="打开"||desc*="获取"||desc*="订阅"||desc*="接收"||desc*="Turn on"||desc*="turn on")&&(desc*="通知"||desc*="推送"||desc*="notifications"||desc*="Notifications")&&(desc!*="定位"&&desc!*="位置"&&desc!*="location"&&desc!*="权限"&&desc!*="成功"&&desc!*="失败"&&(text!*="已"||text*="已知"))]';
 
 const PP_commonTextPatterns =
   '[(text*="是否允许"||text*="申请"||text*="开启"||text*="打开"||text*="获取")&&text*="权限"&&(text!*="定位"&&text!*="位置"&&text!*="location"&&text!*="通知"&&text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))]';
@@ -308,51 +303,35 @@ const globalGroups: RawGlobalGroup[] = [
     rules: [
       {
         key: 0,
-        matches: `${COMMON_PREFIX}${NP_commonTextPatterns} <n * > ${NEGATION_PART_RULE_TEXT}`,
+        matches: [
+          `${COMMON_PREFIX}[(text*="是否允许"||text*="申请"||text*="开启"||text*="打开"||text*="获取"||text*="获得"||text*="订阅"||text*="接收"||text*="Turn on"||text*="turn on"||text*="Enable"||text*="enable")&&(text*="通知"||text*="推送"||text*="消息"||text*="notification"||text*="Notification")&&(text!*="定位"&&text!*="位置"&&text!*="location"&&(text!*="权限"||text*="通知权限"||text*="通知栏权限")&&text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))]`,
+          `${COMMON_PREFIX}[(((text*="打开"||text*="開啟"||text*="开启"||text*="设置"||text*="允许")&&(text*="通知"||text*="推送"))||text="是"||text="启用"||text="确认"||text="确定"||text="同意"||text="开启"||text="设置"||text="允许"||text="好的"||((text*="去"||text*="立即"||text*="立刻"||text*="马上"||text*="前往"||text*="现在")&&(text*="开启"||text="设置"))||text="尝试一下"||text="开启设置"||text="立即打开"||text="Authorize"||text="Let’s go")&&text.length<10]`,
+          `${NEGATION_PART_RULE_TEXT}`,
+        ],
       },
       {
         key: 1,
-        matches: `${COMMON_PREFIX}${NP_commonTextPatterns} <n * > * >n ${NEGATION_PART_RULE_TEXT}`,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="是否允许"||desc*="申请"||desc*="开启"||desc*="打开"||desc*="获取"||desc*="获得"||desc*="订阅"||desc*="接收"||desc*="Turn on"||desc*="turn on"||desc*="Enable"||desc*="enable")&&(desc*="通知"||desc*="推送"||desc*="消息"||desc*="notification"||desc*="Notification")&&(desc!*="定位"&&desc!*="位置"&&desc!*="location"&&(desc!*="权限"||desc*="通知权限"||desc*="通知栏权限")&&desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))]`,
+          `${COMMON_PREFIX}[(((desc*="打开"||desc*="開啟"||desc*="开启"||desc*="设置"||desc*="允许")&&(desc*="通知"||desc*="推送"))||desc="是"||desc="启用"||desc="确认"||desc="确定"||desc="同意"||desc="开启"||desc="设置"||desc="允许"||desc="好的"||((desc*="去"||desc*="立即"||desc*="立刻"||desc*="马上"||desc*="前往"||desc*="现在")&&(desc*="开启"||desc="设置"))||desc="尝试一下"||desc="开启设置"||desc="立即打开"||desc="Authorize"||desc="Let’s go")&&desc.length<10]`,
+          `${NEGATION_PART_RULE_DESC}`,
+        ],
       },
       {
         key: 2,
-        matches: `${COMMON_PREFIX}${NP_commonTextPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_TEXT}`,
+        matches: [
+          `${COMMON_PREFIX}[(text*="是否允许"||text*="申请"||text*="开启"||text*="打开"||text*="获取"||text*="获得"||text*="订阅"||text*="接收"||text*="Turn on"||text*="turn on"||text*="Enable"||text*="enable")&&(text*="通知"||text*="推送"||text*="消息"||text*="notification"||text*="Notification")&&(text!*="定位"&&text!*="位置"&&text!*="location"&&(text!*="权限"||text*="通知权限"||text*="通知栏权限")&&text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))]`,
+          `${COMMON_PREFIX}[(((text*="打开"||text*="開啟"||text*="开启"||text*="设置"||text*="允许")&&(text*="通知"||text*="推送"))||text="是"||text="启用"||text="确认"||text="确定"||text="同意"||text="开启"||text="设置"||text="允许"||text="好的"||((text*="去"||text*="立即"||text*="立刻"||text*="马上"||text*="前往"||text*="现在")&&(text*="开启"||text="设置"))||text="尝试一下"||text="开启设置"||text="立即打开"||text="Authorize"||text="Let’s go")&&text.length<10]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
       },
       {
         key: 3,
-        matches: `${COMMON_PREFIX}${NP_commonDescPatterns} <n * > ${NEGATION_PART_RULE_DESC}`,
-      },
-      {
-        key: 4,
-        matches: `${COMMON_PREFIX}${NP_commonDescPatterns} <n * > * >n ${NEGATION_PART_RULE_DESC}`,
-      },
-      {
-        key: 5,
-        matches: `${COMMON_PREFIX}${NP_commonDescPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_DESC}`,
-      },
-      {
-        key: 6,
-        matches: `${COMMON_PREFIX}${NP_commonTextPatterns} <n * > ${NEGATION_PART_RULE_BUTTON}`,
-      },
-      {
-        key: 7,
-        matches: `${COMMON_PREFIX}${NP_commonTextPatterns} <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
-      },
-      {
-        key: 8,
-        matches: `${COMMON_PREFIX}${NP_commonTextPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
-      },
-      {
-        key: 9,
-        matches: `${COMMON_PREFIX}${NP_commonDescPatterns} <n * > ${NEGATION_PART_RULE_BUTTON}`,
-      },
-      {
-        key: 10,
-        matches: `${COMMON_PREFIX}${NP_commonDescPatterns} <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
-      },
-      {
-        key: 11,
-        matches: `${COMMON_PREFIX}${NP_commonDescPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="是否允许"||desc*="申请"||desc*="开启"||desc*="打开"||desc*="获取"||desc*="获得"||desc*="订阅"||desc*="接收"||desc*="Turn on"||desc*="turn on"||desc*="Enable"||desc*="enable")&&(desc*="通知"||desc*="推送"||desc*="消息"||desc*="notification"||desc*="Notification")&&(desc!*="定位"&&desc!*="位置"&&desc!*="location"&&(desc!*="权限"||desc*="通知权限"||desc*="通知栏权限")&&desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))]`,
+          `${COMMON_PREFIX}[(((desc*="打开"||desc*="開啟"||desc*="开启"||desc*="设置"||desc*="允许")&&(desc*="通知"||desc*="推送"))||desc="是"||desc="启用"||desc="确认"||desc="确定"||desc="同意"||desc="开启"||desc="设置"||desc="允许"||desc="好的"||((desc*="去"||desc*="立即"||desc*="立刻"||desc*="马上"||desc*="前往"||desc*="现在")&&(desc*="开启"||desc="设置"))||desc="尝试一下"||desc="开启设置"||desc="立即打开"||desc="Authorize"||desc="Let’s go")&&desc.length<10]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
       },
     ],
     // 将 Set 转换为数组，并设置 enable 为 false
