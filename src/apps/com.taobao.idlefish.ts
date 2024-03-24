dd4e9a82fd40d56a1c81dde179e24e04c0ab901d
aoguai <1340219674@qq.com>
2024-03-24 16:18:36 +0800

    feat(闲鱼): 红包弹窗, 搜索结果页广告

--- src/apps/com.taobao.idlefish.ts
+++ src/apps/com.taobao.idlefish.ts
@@ -15,5 +15,41 @@ export default defineAppConfig({
       rules: '[desc^="开启系统通知"] > ImageView[clickable=true][desc=null]',
       snapshotUrls: 'https://i.gkd.li/import/13538351',
     },
+    {
+      key: 4,
+      name: '全屏广告-红包弹窗',
+      desc: '点击关闭',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.taobao.idlefish.maincontainer.activity.MainActivity',
+      rules:
+        'WebView[text="Rax App"] > [id="root"] >6 View[index=2][clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/i/14551046',
+    },
+    {
+      key: 5,
+      name: '分段广告-搜索结果页广告',
+      activityIds:
+        'com.idlefish.flutterbridge.flutterboost.boost.FishFlutterBoostActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '@[clickable=true] > [desc$="广告"]',
+          excludeMatches: '@[clickable=true] > [desc^="反馈成功"]',
+          action: 'longClick',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14723597',
+            'https://i.gkd.li/i/14723718', // excludeMatches
+          ],
+        },
+        {
+          key: 1,
+          preKeys: 0,
+          matches:
+            '[desc^="不喜欢该商品"] > ImageView[index=1][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14723632',
+        },
+      ],
+    },
   ],
 });
