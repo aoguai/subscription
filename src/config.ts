871af2b31456d76e87a6937e5f3cc0172b96b509
aoguai <1340219674@qq.com>
2024-03-27 22:12:09 +0800

    chore: 使用 checkUpdateUrl 相对路径(gkd-kit/discussions/558)

--- src/config.ts
+++ src/config.ts
@@ -9,10 +9,7 @@ const subsConfig: RawSubscription = {
   name: '奥怪的GKD订阅',
   author: 'aoguai',
   supportUri: 'https://github.com/aoguai/subscription/tree/custom',
-  updateUrl:
-    'https://raw.githubusercontent.com/aoguai/subscription/custom/dist/aoguai_gkd.json5',
-  checkUpdateUrl:
-    'https://raw.githubusercontent.com/aoguai/subscription/custom/dist/aoguai_gkd.version.json',
+  checkUpdateUrl: './aoguai_gkd.version.json',
   globalGroups,
   categories,
   apps,
