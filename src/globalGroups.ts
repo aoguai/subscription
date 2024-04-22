5a00dbded34849705170c90c291ad53da8f81132
aoguai <1340219674@qq.com>
2024-04-22 11:51:40 +0800

    feat: 全局规则-评价提示

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -59,8 +59,8 @@ const uniqueAppIdsYM = new Set([
 const COMMON_PREFIX =
   '[name!$=".CheckBox"][childCount=0][visibleToUser=true][height>0&&width>0]';
 
-const NEGATION_PART_RULE_TEXT = `${COMMON_PREFIX}[((text^="不"&&text$="谢谢")||text="否"||text="关闭"||text="跳过"||text="关闭按钮"||text="不开启"||text="暂时不用"||text="暂时不要"||text="我已知晓"||text="不用了"||text="本次忽略"||text="考虑一下"||text="考慮一下"||text="先不了"||text="不允许"||text^="不了"||text^="不再"||(text^="稍后"&&text!="稍后再看")||text^="忽略"||text^="暂不"||text^="放弃"||text^="取消"||text$="再说"||text$="拒绝"||text$="再想想"||text$="知道了"||text$="稍后提醒我"||text$="稍後提醒我"||(text^="不"&&text$="謝謝")||text="關閉"||text="關閉按鈕"||text="我已知曉"||text="不開啟"||text$="再說"||text$="拒絕"||text^="暫不"||text~="close"||text~="not now"||text~="(?is)Ignore.*"||text~="(?is)cancel.*"||text~="(?is).*later"||text~="(?is).*refuse"||text~="(?is).*i see")&&(text!*="取消全部")&&text.length<=7]`;
-const NEGATION_PART_RULE_DESC = `${COMMON_PREFIX}[((desc^="不"&&desc$="谢谢")||desc="否"||desc="关闭"||desc="跳过"||desc="关闭按钮"||desc="不开启"||desc="暂时不用"||desc="暂时不要"||desc="我已知晓"||desc="不用了"||desc="本次忽略"||desc="考虑一下"||desc="考慮一下"||desc="先不了"||desc="不允许"||desc^="不了"||desc^="不再"||(desc^="稍后"&&desc!="稍后再看")||desc^="忽略"||desc^="暂不"||desc^="放弃"||desc^="取消"||desc$="再说"||desc$="拒绝"||desc$="再想想"||desc$="知道了"||desc$="稍后提醒我"||desc$="稍後提醒我"||(desc^="不"&&desc$="謝謝")||desc="關閉"||desc="關閉按鈕"||desc="我已知曉"||desc="不開啟"||desc$="再說"||desc$="拒絕"||desc^="暫不"||desc~="close"||desc~="not now"||desc~="(?is)Ignore.*"||desc~="(?is)cancel.*"||desc~="(?is).*later"||desc~="(?is).*refuse"||desc~="(?is).*i see")&&(desc!*="取消全部")&&desc.length<=7]`;
+const NEGATION_PART_RULE_TEXT = `${COMMON_PREFIX}[((text^="不"&&text$="谢谢")||text="否"||text="关闭"||text="跳过"||text="关闭按钮"||text="我没空"||text="不开启"||text="暂时不用"||text="暂时不要"||text="我已知晓"||text="不用了"||text="本次忽略"||text="考虑一下"||text="考慮一下"||text="先不了"||text="不允许"||text^="不了"||text^="不再"||(text^="稍后"&&text!="稍后再看")||text^="忽略"||text^="暂不"||text^="放弃"||text^="取消"||text$="再说"||text$="拒绝"||text$="再想想"||text$="知道了"||text$="稍后提醒我"||text$="稍後提醒我"||(text^="不"&&text$="謝謝")||text="關閉"||text="關閉按鈕"||text="我已知曉"||text="不開啟"||text$="再說"||text$="拒絕"||text^="暫不"||text~="close"||text~="not now"||text~="(?is)Ignore.*"||text~="(?is)cancel.*"||text~="(?is).*later"||text~="(?is).*refuse"||text~="(?is).*i see")&&(text!*="取消全部")&&text.length<=7]`;
+const NEGATION_PART_RULE_DESC = `${COMMON_PREFIX}[((desc^="不"&&desc$="谢谢")||desc="否"||desc="关闭"||desc="跳过"||desc="关闭按钮"||desc="我没空"||desc="不开启"||desc="暂时不用"||desc="暂时不要"||desc="我已知晓"||desc="不用了"||desc="本次忽略"||desc="考虑一下"||desc="考慮一下"||desc="先不了"||desc="不允许"||desc^="不了"||desc^="不再"||(desc^="稍后"&&desc!="稍后再看")||desc^="忽略"||desc^="暂不"||desc^="放弃"||desc^="取消"||desc$="再说"||desc$="拒绝"||desc$="再想想"||desc$="知道了"||desc$="稍后提醒我"||desc$="稍後提醒我"||(desc^="不"&&desc$="謝謝")||desc="關閉"||desc="關閉按鈕"||desc="我已知曉"||desc="不開啟"||desc$="再說"||desc$="拒絕"||desc^="暫不"||desc~="close"||desc~="not now"||desc~="(?is)Ignore.*"||desc~="(?is)cancel.*"||desc~="(?is).*later"||desc~="(?is).*refuse"||desc~="(?is).*i see")&&(desc!*="取消全部")&&desc.length<=7]`;
 const NEGATION_PART_RULE_BUTTON = `${COMMON_PREFIX}[(vid~="(?is).*iv.*"||vid~="(?is).*guide.*"||vid~="(?is).*alert"||vid~="(?is).*notific.*"||vid~="(?is).*dialog.*"||vid~="(?is).*btn.*"||vid~="(?is).*ad.*"||vid~="(?is).*ab.*")&&(vid~="(?is).*close"||vid~="(?is).*delete"||vid~="(?is).*cancel"||vid~="(?is).*cancle"||vid~="(?is).*exit")||vid~="(?is)close.*"||vid~="(?is)ab.*"||vid~="(?is)closeIv.*"||vid~="(?is)ivDelete.*"||vid~="(?is)deleteIv.*"||vid~="(?is).*_close"||text=""||desc=""||text="×"||desc="×"||text="퀺"||desc="퀺"]`;
 
 const PA_commonTextPatterns =
@@ -268,7 +268,7 @@ export default defineGkdGlobalGroups([
         key: 0,
         matches: [
           `${COMMON_PREFIX}[((text*="给"||text*="赏"||text*="来个"||text*="一下"||text*="app"||text*="喜欢")&&(text*="好评"||text*="评分"||text*="评价"||text*="鼓励"||text*="Rate"||text*="吗？"))&&(text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))||name$=".RatingBar"]`,
-          `${COMMON_PREFIX}[((text*="好评"||text*="鼓励一下"||text*="评价"||text*="评分")||(text="确定"||text="提交"||text="满意"||text~="RATE NOW"||text~="(?is).*Love it.*"))&&text.length<9]`,
+          `${COMMON_PREFIX}[((text*="吐槽"||text*="好评"||text*="鼓励一下"||text*="评价"||text*="评分")||(text="确定"||text="提交"||text="满意"||text~="RATE NOW"||text~="(?is).*Love it.*"))&&text.length<9]`,
           `${NEGATION_PART_RULE_TEXT}`,
         ],
       },
@@ -276,7 +276,7 @@ export default defineGkdGlobalGroups([
         key: 1,
         matches: [
           `${COMMON_PREFIX}[((desc*="给"||desc*="赏"||desc*="来个"||desc*="一下"||desc*="app"||desc*="喜欢")&&(desc*="好评"||desc*="评分"||desc*="评价"||desc*="鼓励"||desc*="Rate"||desc*="吗？"))&&(desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))||name$=".RatingBar"]`,
-          `${COMMON_PREFIX}[((desc*="好评"||desc*="鼓励一下"||desc*="评价"||desc*="评分")||(desc="确定"||desc="提交"||desc="满意"||desc~="RATE NOW"||desc~="(?is).*Love it.*"))&&desc.length<9]`,
+          `${COMMON_PREFIX}[((desc*="吐槽"||desc*="好评"||desc*="鼓励一下"||desc*="评价"||desc*="评分")||(desc="确定"||desc="提交"||desc="满意"||desc~="RATE NOW"||desc~="(?is).*Love it.*"))&&desc.length<9]`,
           `${NEGATION_PART_RULE_DESC}`,
         ],
       },
@@ -284,7 +284,7 @@ export default defineGkdGlobalGroups([
         key: 2,
         matches: [
           `${COMMON_PREFIX}[((text*="给"||text*="赏"||text*="来个"||text*="一下"||text*="app"||text*="喜欢")&&(text*="好评"||text*="评分"||text*="评价"||text*="鼓励"||text*="Rate"||text*="吗？"))&&(text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))||name$=".RatingBar"]`,
-          `${COMMON_PREFIX}[((text*="好评"||text*="鼓励一下"||text*="评价"||text*="评分")||(text="确定"||text="提交"||text="满意"||text~="RATE NOW"||text~="(?is).*Love it.*"))&&text.length<9]`,
+          `${COMMON_PREFIX}[((text*="吐槽"||text*="好评"||text*="鼓励一下"||text*="评价"||text*="评分")||(text="确定"||text="提交"||text="满意"||text~="RATE NOW"||text~="(?is).*Love it.*"))&&text.length<9]`,
           `${NEGATION_PART_RULE_BUTTON}`,
         ],
       },
@@ -292,7 +292,7 @@ export default defineGkdGlobalGroups([
         key: 3,
         matches: [
           `${COMMON_PREFIX}[((desc*="给"||desc*="赏"||desc*="来个"||desc*="一下"||desc*="app"||desc*="喜欢")&&(desc*="好评"||desc*="评分"||desc*="评价"||desc*="鼓励"||desc*="Rate"||desc*="吗？"))&&(desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))||name$=".RatingBar"]`,
-          `${COMMON_PREFIX}[((desc*="好评"||desc*="鼓励一下"||desc*="评价"||desc*="评分")||(desc="确定"||desc="提交"||desc="满意"||desc~="RATE NOW"||desc~="(?is).*Love it.*"))&&desc.length<9]`,
+          `${COMMON_PREFIX}[((desc*="吐槽"||desc*="好评"||desc*="鼓励一下"||desc*="评价"||desc*="评分")||(desc="确定"||desc="提交"||desc="满意"||desc~="RATE NOW"||desc~="(?is).*Love it.*"))&&desc.length<9]`,
           `${NEGATION_PART_RULE_BUTTON}`,
         ],
       },
