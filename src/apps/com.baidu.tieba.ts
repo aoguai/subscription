e46f0f081d5800ce17b8c9bd9337a8dcf5c45fc1
aoguai <1340219674@qq.com>
2024-04-24 16:51:02 +0800

    fix(百度贴吧): 贴吧内签到并关闭弹窗

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -216,18 +216,23 @@ export default defineGkdApp({
         {
           key: 0,
           name: '贴吧页签到',
-          activityIds: ['com.baidu.tieba.forum.ForumActivity'],
-          matches:
-            'WebView[text="frs"] > View > View > View > View > TextView[text="签到"][clickable=true]',
-          snapshotUrls: ['https://i.gkd.li/import/13776801'],
+          activityIds: 'com.baidu.tieba.forum.ForumActivity',
+          matches: '@TextView[text="签到"][visibleToUser=true] <<n WebView[text="frs"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13776801',
+            'https://i.gkd.li/i/15087289',
+          ],
         },
         {
+          preKeys: 0,
           key: 1,
-          name: '签到成功-弹窗',
-          activityIds: ['com.baidu.tbadk.browser.TBWebContainerActivity'],
-          matches:
-            'WebView[text="签到弹窗"] > View > View > TextView[text=""][clickable=true]',
-          snapshotUrls: ['https://i.gkd.li/import/13776424'],
+          name: '关闭签到成功弹窗',
+          activityIds: 'com.baidu.tbadk.browser.TBWebContainerActivity',
+          matches: 'WebView[text="签到弹窗"] > View > View > TextView[text=""][index=2][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13776424',
+            'https://i.gkd.li/i/15087327',
+          ],
         },
       ],
     },
