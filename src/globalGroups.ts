f8d2ac67325c04414b6dc3ebf192992e1d902b32
aoguai <1340219674@qq.com>
2024-07-28 20:43:56 +0800

    feat: 全局规则-开屏广告

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -83,13 +83,13 @@ export default defineGkdGlobalGroups([
       {
         key: 0,
         excludeMatches:
-          '[text*="搜索" || vid~="(?is).*search.*" || desc*="搜索"][visibleToUser=true]', // 防止在应用的搜索页面误触
+          '[(text*="搜索" && text.length<6) || vid~="(?is).*search.*" || (desc*="搜索" && desc.length<6)][visibleToUser=true]', // 防止在应用的搜索页面误触
         matches: `${COMMON_PREFIX}[(text.length<10&&(text*="跳过"||text*="跳過"||text~="(?is).*skip.*"))||id~="(?is).*tt_splash_skip_btn"||vid~="(?is).*skip.*"||(vid~="(?is).*count.*"&&vid~="(?is).*down.*"&&!(vid~="(?is).*load.*")&&!(vid~="(?is).*hour.*")&&!(vid~="(?is).*minute.*")&&!(vid~="(?is).*add.*")&&!(vid~="(?is).*ead.*"))||desc*="跳过"||desc~="(?is).*skip.*"][!(text~="([01]?[0-9]|2[0-3])[:：][0-5][0-9]")][!(desc~="([01]?[0-9]|2[0-3])[:：][0-5][0-9]")]`,
       },
       {
         key: 1,
         excludeMatches:
-          '[text*="搜索" || vid~="(?is).*search.*" || desc*="搜索"][visibleToUser=true]', // 防止在应用的搜索页面误触
+          '[(text*="搜索" && text.length<6) || vid~="(?is).*search.*" || (desc*="搜索" && desc.length<6)][visibleToUser=true]', // 防止在应用的搜索页面误触
         matches:
           'FrameLayout[visibleToUser=true][height>0&&width>0][childCount>=2] > FrameLayout[visibleToUser=true][height>0&&width>0][childCount>2] > @View[clickable=true][height>0&&width>0][visibleToUser=true] + TextView[clickable=true][height>0&&width>0][visibleToUser=true]', // 字节 SDK
       },
