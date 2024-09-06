e317d4286670331c1605a2786934e88a16ed494e
aoguai <1340219674@qq.com>
2024-09-06 18:55:45 +0800

    feat(PU口袋校园): 弹窗广告

--- src/apps/com.xyhui.ts
+++ src/apps/com.xyhui.ts
@@ -41,12 +41,16 @@ export default defineGkdApp({
         {
           key: 2,
           name: '快手广告',
-          activityIds: 'com.xyhui.start.LoadingActivity',
-          matches:
-            'ImageView < @ViewGroup[clickable=true] < * <2 * + * >3 [text="广告"]',
+          fastQuery: true,
+          activityIds: [
+            'com.xyhui.start.PUMainActivity',
+            'com.xyhui.start.LoadingActivity',
+          ],
+          matches: 'ImageView < @ViewGroup[clickable=true] < * <2 * +n * >(1,3) [text="广告"]',
           snapshotUrls: [
-            'https://i.gkd.li/import/13259196',
             'https://i.gkd.li/import/13259198',
+            'https://i.gkd.li/i/16837806',
+            'https://i.gkd.li/i/13259196',
           ],
         },
         {
