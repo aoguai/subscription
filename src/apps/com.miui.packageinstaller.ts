bb2a355c97551d004538bf8e207e57debba8889f
aoguai <1340219674@qq.com>
2024-11-10 16:51:19 +0800

    perf(应用包管理组件): 放弃开启安全守护, 自动安装/更新应用, 自动允许应用安装软件

--- src/apps/com.miui.packageinstaller.ts
+++ src/apps/com.miui.packageinstaller.ts
@@ -5,196 +5,8 @@ export default defineGkdApp({
   name: '应用包管理组件',
   groups: [
     {
-      enable: false,
-      key: 9,
-      name: '功能类-自动第三方安装应用',
-      desc: '自动允许第三方应用调用安装，安装应用',
-      rules: [
-        {
-          key: 0,
-          activityIds: [],
-          matches:
-            'TextView[text*="尝试安装应用"] < LinearLayout +2n LinearLayout > Button[text="继续"]',
-          snapshotUrls: 'https://i.gkd.li/import/12874746',
-        },
-        {
-          key: 1,
-          activityIds: [
-            'com.miui.packageInstaller.ui.InstallPrepareAlertActivity',
-            'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          ],
-          matches: '@[text="允许"] + [text="禁止"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13054478',
-            'https://i.gkd.li/import/13399425',
-          ],
-        },
-      ],
-    },
-    {
-      key: 10,
-      name: '功能类-自动安装应用',
-      desc: '启用后安装高风险应用将自动完成，请自行评估风险决定是否启用',
-      enable: false,
-      fastQuery: true,
-      rules: [
-        // 正常安装
-        {
-          key: 0,
-          name: '点击【继续安装】',
-          activityIds: [
-            'com.miui.packageInstaller.ui.InstallPrepareAlertActivity',
-            'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          ],
-          matches: ['[text="取消安装"]', '[text="继续安装"]'],
-          snapshotUrls: [
-            'https://i.gkd.li/import/12818034',
-            'https://i.gkd.li/import/12818054',
-            'https://i.gkd.li/i/12889120',
-          ],
-        },
-        {
-          key: 1,
-          name: '点击【继续】',
-          activityIds: 'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          matches: '[text="取消"] - @*[clickable=true] >2 [text="继续"]',
-          snapshotUrls: 'https://i.gkd.li/i/14392314',
-        },
-        // 需勾选【已了解此应用未经安全检测】才能继续安装
-        {
-          key: 2,
-          name: '勾选【已了解此应用未经安全检测】',
-          activityIds: 'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          matches: '[text^="已了解此"][checked=false]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12888410', // 未勾选
-            'https://i.gkd.li/import/12889120', // 已勾选
-            'https://i.gkd.li/i/16171390',
-          ],
-        },
-
-        // 需勾选【已了解此应用存在高风险】才能继续安装
-        {
-          key: 3,
-          name: '点击【了解风险】',
-          activityIds: 'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          matches: '@[text="了解风险"] + [text="取消安装"]',
-          snapshotUrls: 'https://i.gkd.li/import/12889135',
-        },
-        {
-          preKeys: 3,
-          key: 4,
-          name: '勾选【已了解此应用存在高风险】',
-          activityIds: 'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          matches:
-            '[id="com.miui.packageinstaller:id/install_checked"][checked=false]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12889137', // 未勾选
-            'https://i.gkd.li/import/12889148', // 已勾选
-          ],
-        },
-        {
-          preKeys: 4,
-          key: 5,
-          name: '点击【仍然安装】',
-          activityIds: [
-            'com.miui.packageInstaller.NewInstallerPrepareActivity',
-            'com.miui.packageInstaller.ui.InstallPrepareAlertActivity',
-          ],
-          matches: '[text="仍然安装"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12889148',
-            'https://i.gkd.li/i/16187625',
-          ],
-        },
-
-        // 安装完成
-        {
-          key: 6,
-          name: '点击【完成】',
-          activityIds: [
-            'com.miui.packageInstaller.ui.normalmode.InstallProgressActivity',
-            'com.miui.packageInstaller.InstallProgressActivity',
-            'com.miui.packageInstaller.ui.securemode.PureInstallProgressActivity',
-          ],
-          matches:
-            '[id="com.miui.packageinstaller:id/done_layout"] > [text="完成"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12818044', // com.miui.packageInstaller.ui.normalmode.InstallProgressActivity
-            'https://i.gkd.li/import/13229404', // com.miui.packageInstaller.InstallProgressActivity
-            'https://i.gkd.li/import/13501872', // com.miui.packageInstaller.ui.securemode.PureInstallProgressActivity
-          ],
-        },
-      ],
-    },
-    {
-      enable: false,
-      key: 11,
-      name: '功能类-自动更新应用',
-      rules: [
-        // 正常更新
-        {
-          key: 0,
-          name: '点击【继续更新】',
-          activityIds: [
-            'com.miui.packageInstaller.ui.InstallPrepareAlertActivity',
-            'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          ],
-          matches: ['[text="取消更新"]', '[text="继续更新"]'],
-          snapshotUrls: [
-            'https://i.gkd.li/import/12817988',
-            'https://i.gkd.li/import/12910080',
-            'https://i.gkd.li/i/14392274',
-          ],
-        },
-        {
-          key: 1,
-          name: '点击【继续更新】-2',
-          activityIds: 'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          matches: '@[text="继续更新"] + [text="取消更新"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13024731',
-            'https://i.gkd.li/import/13038465',
-          ],
-        },
-
-        // 需勾选【已了解此安装包未经安全检测】才能继续更新
-        {
-          key: 2,
-          name: '勾选【已了解此安装包未经安全检测】',
-          activityIds: [
-            'com.miui.packageInstaller.ui.InstallPrepareAlertActivity',
-            'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          ],
-          matches: '[text="已了解此安装包未经安全检测"][checked=false]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13024730', // 未勾选
-            'https://i.gkd.li/import/13024731', // 已勾选
-            'https://i.gkd.li/i/16171390',
-          ],
-        },
-
-        // 更新完成
-        {
-          key: 3,
-          name: '点击【完成】',
-          activityIds: [
-            'com.miui.packageInstaller.ui.normalmode.InstallProgressActivity',
-            'com.miui.packageInstaller.InstallProgressActivity',
-          ],
-          matches:
-            '[id="com.miui.packageinstaller:id/done_layout"] > [text="完成"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12817999',
-            'https://i.gkd.li/import/13255733',
-          ],
-        },
-      ],
-    },
-    {
-      key: 12,
+      key: 7,
       name: '功能类-放弃开启安全守护',
-      enable: false,
       fastQuery: true,
       activityIds:
         'com.miui.packageInstaller.ui.normalmode.InstallProgressActivity',
@@ -202,15 +14,67 @@ export default defineGkdApp({
         {
           key: 0,
           matches: '[text="30天内不再提示"][checked=false]',
-          exampleUrls: 'https://e.gkd.li/fe6682cd-a27a-4183-8717-15fd373bafcf',
           snapshotUrls: 'https://i.gkd.li/i/16487140',
         },
         {
           preKeys: [0],
           key: 1,
           matches: '[text="放弃"]',
-          exampleUrls: 'https://e.gkd.li/859dac99-62ca-4876-bdfb-c84795254111',
           snapshotUrls: 'https://i.gkd.li/i/16487142',
+        },
+      ],
+    },
+    {
+      key: 13,
+      name: '功能类-自动安装/更新应用',
+      desc: '该规则仅适配关闭小米系统[安全守护]的情况',
+      fastQuery: true,
+      rules: [
+        {
+          key: 0,
+          name: '点击[安装]/[更新]',
+          activityIds: 'com.miui.packageInstaller.NewInstallerPrepareActivity',
+          matches:
+            '@FrameLayout[clickable=true] > LinearLayout[childCount=1] > [text^="继续"][text.length=4]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/16487278',
+            'https://i.gkd.li/i/16487282',
+          ],
+        },
+        {
+          // preKeys: [0], 有概率不触发
+          key: 1,
+          name: '点击[完成]',
+          activityIds:
+            'com.miui.packageInstaller.ui.normalmode.InstallProgressActivity',
+          matches: '[text="完成"]',
+          snapshotUrls: 'https://i.gkd.li/i/16487274',
+        },
+      ],
+    },
+    {
+      key: 14,
+      name: '功能类-自动允许应用安装软件',
+      fastQuery: true,
+      activityIds: 'com.miui.packageInstaller.ui.InstallPrepareAlertActivity',
+      rules: [
+        {
+          key: 0,
+          name: '点击[记住我的选择]',
+          matches: [
+            '[text$="安装应用？"]',
+            'CheckBox[text="记住我的选择"][checked=false]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/16487366',
+            'https://i.gkd.li/i/16487389',
+          ],
+        },
+        {
+          preKeys: [0],
+          name: '点击[允许]',
+          matches: '[text="允许"]',
+          snapshotUrls: 'https://i.gkd.li/i/16487365',
         },
       ],
     },
