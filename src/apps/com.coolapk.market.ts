0c14e8e933e1cb85bc574f79236dfeebf09938d1
aoguai <1340219674@qq.com>
2024-11-16 22:11:02 +0800

    perf(酷安): 信息流广告

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -9,6 +9,7 @@ export default defineGkdApp({
       name: '分段广告-卡片广告',
       desc: '点击卡片右上角按钮->免广告-点击关闭->选择关闭原因-点击不感兴趣',
       enable: false,
+      matchRoot: true,
       fastQuery: true,
       activityIds: [
         'com.coolapk.market.view.main.MainActivity', // 缺少快照
