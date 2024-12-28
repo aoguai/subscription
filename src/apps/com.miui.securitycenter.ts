e66566680fb08cb9ccb6c985b64e7bc991a3665a
aoguai <1340219674@qq.com>
2024-12-28 17:10:10 +0800

    feat(小米手机管家): 使用返回退出时直接点击[退出]

--- src/apps/com.miui.securitycenter.ts
+++ src/apps/com.miui.securitycenter.ts
@@ -43,32 +43,22 @@ export default defineGkdApp({
     },
     {
       key: 12,
-      name: '功能类-忽略体检优化提示',
+      name: '功能类-使用返回退出时直接点击[退出]',
       desc: '退出时忽略[体检优化分数]/忽略[存储空间预警]',
-      enable: false,
-      fastQuery: true,
-      activityIds: 'com.miui.securityscan.MainActivity',
       rules: [
         {
-          key: 1,
-          name: '忽略优化分数',
+          fastQuery: true,
+          activityIds: 'com.miui.securityscan.MainActivity',
           matches: [
-            '[text^="手机体检分数不足"][id$="id/message"]',
-            '[text="退出"]',
+            '[text="体检优化" || text="存储空间预警"][visibleToUser=true]',
+            '[text="退出"][visibleToUser=true]',
           ],
           snapshotUrls: [
-            'https://i.gkd.li/import/13474504', // app版本v5
-            'https://i.gkd.li/import/13476770', // app版本v8
+            'https://i.gkd.li/i/13474504',
+            'https://i.gkd.li/i/13476770',
+            'https://i.gkd.li/i/15137908',
           ],
-        },
-        {
-          key: 2,
-          name: '忽略存储空间预警',
-          matches: [
-            '[text$="释放存储空间"][id$="id/message"]',
-            '[text="退出"]',
-          ],
-          snapshotUrls: ['https://i.gkd.li/i/15137908'],
+          excludeSnapshotUrls: 'https://i.gkd.li/i/18126157',
         },
       ],
     },
