998645a56fa2c77e2cf8fc39413174d8ed02a2c8
aoguai <1340219674@qq.com>
2025-03-29 11:48:19 +0800

    feat(阿里云盘): 自动签到

--- src/apps/com.alicloud.databox.ts
+++ src/apps/com.alicloud.databox.ts
@@ -25,23 +25,22 @@ export default defineGkdApp({
           matches: '[text="领取"][clickable=true][visibleToUser=true]',
           snapshotUrls: [
             'https://i.gkd.li/import/12929318',
-            'https://i.gkd.li/i/15573070',
             'https://i.gkd.li/i/15942837',
+            'https://i.gkd.li/i/15573070', // 签到前
+            'https://i.gkd.li/i/15573233', // 签到后
           ],
         },
         {
           key: 1,
-          preKeys: [0],
           name: '在签到后，关闭弹窗',
           action: 'back',
           anyMatches: [
             '[vid="ivCardBackBackground"][visibleToUser=true]',
-            '[vid="ivClose"][visibleToUser=true]',
+            '[vid="cvCardFront"] > [vid="ivClose"][visibleToUser=true]',
           ],
           snapshotUrls: [
             'https://i.gkd.li/i/18108340',
-            'https://i.gkd.li/i/15573070', // 签到前
-            'https://i.gkd.li/i/15573233', // 签到后
+            'https://i.gkd.li/i/19515122',
           ],
         },
       ],
